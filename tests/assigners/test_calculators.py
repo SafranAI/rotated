7e4d89150a499aceba76d6be51b72bd210db59a2
Max19475 <maxime.arbelot@preligens.com>
2025-10-23 16:51:38 +0200

    Test uv cache and failure

--- tests/assigners/test_calculators.py
+++ tests/assigners/test_calculators.py
@@ -22,6 +22,8 @@ def test_rotated_iou_calculator():
     empty_iou2 = calc(boxes1, empty_boxes)
     assert empty_iou2.shape == (2, 0)
 
+def fake_invalid_test():
+    assert False, "heheh"
 
 def test_horizontal_iou_calculator():
     """Test horizontal IoU calculator basic functionality."""
