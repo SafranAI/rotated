ea5cbf4f7fae9dbcccdb94ba4831533c1f3ccfdf
Vincent Camenen <91896639+VCamen@users.noreply.github.com>
2025-11-27 16:08:32 +0100

    Fix imports

--- src/rotated/models/__init__.py
+++ src/rotated/models/__init__.py
@@ -1,3 +1,3 @@
-from rotated.models.ppyoloer import PPYOLOER, create_ppyoloer_model
+from rotated.models.ppyoloer import PPYOLOER, PPYOLOER_CONFIGS, PPYOLOERSize, create_ppyoloer_model
 
-__all__ = ["create_ppyoloer_model", "PPYOLOER"]
+__all__ = ["create_ppyoloer_model", "PPYOLOER", "PPYOLOERSize", "PPYOLOER_CONFIGS"]
